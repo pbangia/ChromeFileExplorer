97fb3223b2fac7c5147401f7d56fbaf9cbe78104
Londe <ytso868@aucklanduni.ac.nz>
2017-04-17 21:25:02 +1200

    Added jquery widget and tested a basic data tree structure view

---
+++ tree.js
@@ -0,0 +1,21 @@
+var data = [
+    {
+        name: 'node1',
+        children: [
+            { name: 'child1' },
+            { name: 'child2' }
+        ]
+    },
+    {
+        name: 'node2',
+        children: [
+            { name: 'child3' }
+        ]
+    }
+];
+
+$(function () {
+    $('#tree1').tree({
+        data: data
+    });
+});
