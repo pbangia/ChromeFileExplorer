b3d11a661289f84eec67dec506bf8ec99492ce11
Barakib <Barakib@aucklanduni.ac.nz>
2017-04-20 17:32:57 +1200

    Added side menu toggle

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-  	
-	/* function to enable/disable buttons if file is selected */
-  	function disableButtons(boolean){
-  		$("#buttons :button").attr("disabled", boolean);
-  	}
 
-  	$( document ).ready(function() {
+/* function to enable/disable buttons if file is selected */
+function disableButtons(boolean){
+	$("#buttons :button").attr("disabled", boolean);
+}
+
+$( document ).ready(function() {
     	//disable buttons on load
     	disableButtons(true);
-	});
+    });
+
+/* toggles side menu when hamburger clicked */
+function toggleSideMenu() {
+	$( ".side-menu" ).animate( {'width': 'toggle'});
+}
