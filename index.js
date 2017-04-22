bce1b19ad79c74bac5faab55fb0e0af94ea25058
Barakib <pban945@aucklanduni.ac.nz>
2017-04-22 22:21:52 +1200

    Reading and parsing content from native explorer to main content view

--- index.js
+++ index.js
@@ -7,9 +7,28 @@ function disableButtons(boolean){
 $( document ).ready(function() {
     	//disable buttons on load
     	disableButtons(true);
-    });
+ 
+});
 
 /* toggles side menu when hamburger clicked */
 function toggleSideMenu() {
 	$( ".side-menu" ).animate( {'width': 'toggle'});
+}
+
+$(function () {
+    $('.btn-radio').click(function(e) {
+        $('.btn-radio').not(this).removeClass('active')
+    		.siblings('input').prop('checked',false)
+            .siblings('.img-radio').css('opacity','0.5');
+    	$(this).addClass('active')
+            .siblings('input').prop('checked',true)
+    		.siblings('.img-radio').css('opacity','1');
+    });
+});
+
+function changeDir(folderItem) {
+	var caption = folderItem.getElementsByClassName('caption')[0];
+	var path = caption.getAttribute('name');
+	console.log('Clicked on folder/file with path: ' + path);
+	$(folderItem).addClass('selected');
 }
