835f9b74ce0c06488a2f75c7a28c0bc089586f9b
Forlonde <jcoo418@aucklanduni.ac.nz>
2017-04-24 10:54:05 +1200

    Moved drag and drop functionality to index.js
    This was needed as the content.js file they were previously in cannot
    access the DOM.

--- index.js
+++ index.js
@@ -58,4 +58,66 @@ function copy(event, item){
 	console.log('Copying: ' + path);
 }
 
+var idgenerator = 0;
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+
+	var draggedItem = document.getElementById(ev.dataTransfer.getData("text"));
+  	//If the image is dragged onto a div containing an image
+  	if ((ev.target instanceof HTMLImageElement)||(ev.target instanceof HTMLDivElement)){
+	  	var newDiv;	// Clone the original div
+	  	
+      	//newDiv = createDiv("newdiv"+idgenerator);   //Create a new div
+      	//newDiv.appendChild(image);                      //Add the old image to it
+				/*
+		if (ev.target instanceof HTMLImageElement){
+		  	newDiv = draggedItem.parentElement.cloneNode(true);
+			  newDiv.id = (idgenerator);
+	  		idgenerator++;
+          	ev.target.parentNode.insertAdjacentHTML('afterend', newDiv.outerHTML);   //And put it to the side of the target image's div.
+		}else{
+			*/
+			newDiv = draggedItem.cloneNode(true);
+			newDiv.id = idgenerator;
+	  	idgenerator++;
+
+			draggedItem.parentElement.removeChild(draggedItem);
+
+			if ($(ev.target).hasClass("folderItem")){
+				ev.target.insertAdjacentHTML('afterend', newDiv.outerHTML);
+			}else{
+				// Note that if we put divs inside the folder item divs, this will stop working
+				ev.target.parentNode.insertAdjacentHTML('afterend', newDiv.outerHTML);
+			}
+
+         //And put it to the side of the targeted div.
+        //}
+		/* Made redundant by cloning
+        document.getElementById("newdiv"+(idgenerator-1)).addEventListener('drop', function(ev) {drop(ev)}, false);
+        document.getElementById("newdiv"+(idgenerator-1)).addEventListener('dragover', function(ev) {allowDrop(ev)}, false);
+        document.getElementById("newdiv"+(idgenerator-1)).addEventListener('dragstart', function(ev) {drag(ev)}, false);
+		*/
+    }else{
+		// New div structure means this won't work
+      	// ev.target.appendChild(image);   //If the div is blank, put the new image in it. 
+    }
+
+		/*
+    for (var i=0; i<divs.length; i++){
+      if (divs[i].innerHTML===""){
+        var divToRemove = document.getElementById(divs[i].id);
+        divToRemove.parentNode.removeChild(divToRemove);
+        break;
+      }
+    }
+		*/
+  }
