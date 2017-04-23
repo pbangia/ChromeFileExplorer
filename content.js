551743f613a6a11674df8b379578ce7ca83a5c2d
Priyankit Bangia <priyankit.bangia@gmail.com>
2017-04-23 13:50:06 +1200

    Set fIle icons to change with file type

--- content.js
+++ content.js
@@ -66,6 +66,13 @@ function readFiles() {
           return reloadFolders(param);
         }
       })(), false);
+    } else { 
+      //if file, set appropriate file icon
+      var img = fvClone.getElementsByTagName('img')[0];
+      var imgPath = fileName.split(".");
+      var extension = imgPath[imgPath.length-1] + '.png';
+      if (!fileTypeIcons[extension]) extension = 'file.png';
+      img.setAttribute("src", 'fileTypeIcons/'+extension);
     }
 
     contentList.appendChild(fvClone);
@@ -226,6 +233,49 @@ function reloadFolders(path){
     console.log("Current Dir:" + currentDirectory);
     //addNewDivs(5);
   });
+}
+
+/* File extension names for icons */
+var fileTypeIcons = 
+{
+    'ai.png':true,
+    'audition.png':true,
+    'avi.png':true,
+    'bridge.png':true,
+    'css.png':true,
+    'csv.png':true,
+    'dbf.png':true,
+    'doc.png':true,
+    'dreamweaver.png':true,
+    'dwg.png':true,
+    'exe.png':true,
+    'file.png':true,
+    'fireworks.png':true,
+    'fla.png':true,
+    'flash.png':true,
+    'html.png':true,
+    'illustrator.png':true,
+    'indesign.png':true,
+    'iso.png':true,
+    'jpg.png':true,
+    'js.png':true,
+    'json.png':true,
+    'mp3.png':true,
+    'mp4.png':true,
+    'pdf.png':true,
+    'photoshop.png':true,
+    'png.png':true,
+    'ppt.png':true,
+    'prelude.png':true,
+    'premiere.png':true,
+    'psd.png':true,
+    'rtf.png':true,
+    'search.png':true,
+    'svg.png':true,
+    'txt.png':true,
+    'xls.png':true,
+    'xml.png':true,
+    'zip.png':true         
 }
 
 
