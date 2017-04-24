bc12f602a48c4552f78496bfd454a2e0374de4e0
Dentonen <gtio848@aucklanduni.ac.nz>
2017-04-24 16:51:12 +1200

    Refactored content.js: Organised code order, moved some variables into resources.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {
-  extension_path : "C:/Users/gtio848/AndroidStudio Projects/Wobury",
-  default_path : "C:/Users/gtio848"
+  extension_path : "C:/Users/gtio848/AndroidStudio Projects/Wobury/",
+  default_path : "C:/Users/gtio848/"
 };
